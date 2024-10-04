9517b75fcb2b86216b14811ba173450fc628e5d2
DHEERAJ SINGH <31558540+dheeraj2112@users.noreply.github.com>
2024-10-04 21:00:53 +0530

    Initial Snowflake connector demo AWS Lambda code

--- AWS_Snowflake_demo_function.py
+++ AWS_Snowflake_demo_function.py
@@ -22,18 +22,18 @@ def lambda_handler(event, context):
     conn=sf.connect(user=user,password=password,account=account);
     print(conn);
 
-    statement_1='USE WAREHOUSE '+warehouse;
-    statement3="USE DATABASE "+database;
-    statement3_1="USE SCHEMA "+schema;
+    statement1='USE WAREHOUSE '+warehouse;
+    statement2="USE DATABASE "+database;
+    statement3="USE SCHEMA "+schema;
     statement4="USE ROLE "+role;
     print('------Executing Context Queries-------');
     print('');
-    run_query(conn,statement_1);
-    print(statement_1);
+    run_query(conn,statement1);
+    print(statement1);
+    run_query(conn,statement2);
+    print(statement3);
     run_query(conn,statement3);
     print(statement3);
-    run_query(conn,statement3_1);
-    print(statement3_1);
     run_query(conn,statement4);
     print(statement4);
     print('------Executing Main Queries-------');
@@ -41,7 +41,7 @@ def lambda_handler(event, context):
     print(table_creation);
     run_query(conn, table_creation);
     data_inserts="INSERT INTO DEMO_TABLE(COL1, COL2) VALUES (123, 'TEST1'),(234, 'TEST2'), (456, 'TEST3')";
-    run_query(data_inserts);
+    run_query(conn,data_inserts);
     print(data_inserts);
     print('');
     print('------Execution Successful--------');
